0327d60ac6d9d2ca0bf48e8d7d1695314541683e
Igor Freitas Sym <igorsym97@gmail.com>
2018-11-30 15:17:51 -0200

    Set root to users#index as a test

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   resources :reports
   resources :events
   resources :users
+
+  root to: "users#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
