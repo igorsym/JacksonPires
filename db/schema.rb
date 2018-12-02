aa08277c872fce1b5b2fb2eef22db81ab3253828
Igor Freitas Sym <igorsym97@gmail.com>
2018-12-01 22:43:34 -0200

    test_check

--- db/schema.rb
+++ db/schema.rb
@@ -78,7 +78,7 @@ ActiveRecord::Schema.define(version: 2018_11_30_161309) do
     t.string "phone"
     t.date "birth_date"
     t.boolean "gender"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "extension"
