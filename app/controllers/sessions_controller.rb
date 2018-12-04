2e2a213c779ed248249e6d8da67de643bafbf425
Vitor Sternlicht <mvitsor@gmail.com>
2018-12-04 15:27:41 -0200

    logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,8 @@ class SessionsController < ApplicationController
       render "new"
     end
   end
-  def new
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Logged out!"
   end
 end
